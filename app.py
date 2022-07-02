d5efe578ffebd4dce0b3f39c9df813966f1345df
dk260811 <donkokaj@DESKTOP-H394OOE.localdomain>
2022-07-02 22:08:13 +0200

    added Procfile

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from pickle import NONE
 from telnetlib import PRAGMA_HEARTBEAT, PRAGMA_LOGON
 from tkinter.messagebox import NO
-
+import tkinter
   
 # C:\Users\Lenovo\pyver\py3105\lib\tty.py qetu e kom hek termios
 
